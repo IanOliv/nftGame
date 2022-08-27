4a95cf7656919a4ef288ba758e7ed91c039917c6
Ian Oliveira <22185362+IanOliv@users.noreply.github.com>
2022-08-27 14:07:29 -0300

    feat: add character struct

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,8 +5,37 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-  constructor() {
-    //nao pode usar acentos nos arquivos .sol, pois dah ruim!
-    console.log("Esse eh o contrato do meu jogo, vamo!");
-  }
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) {
+        for (uint256 i = 0; i < characterNames.length; i++) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+			CharacterAttributes memory c = defaultCharacters[i];
+			console.log("Personagem inicializado: %s com %s de HP, img %s",
+			    c.name, c.hp, c.imageURI);
+        }
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,78 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
-  await gameContract.deployed();
-  console.log("Contrato implantado no endereço:", gameContract.address);
+	let characters = [
+		{
+			name: "Ruby",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/9/9b/Ruby_Form_Two_by_AireDaleDogz.png/revision/latest?cb=20200907180423&path-prefix=pt-br",
+			hp: 300,
+			atck: 30
+		},
+		{
+			name: "Safira",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/0/07/Sapphire_Form_Two_by_BlueLys.png/revision/latest?cb=20200907180735&path-prefix=pt-br",
+			hp: 200,
+			atck: 120
+		},
+		{
+			name: "Perola",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/c/cf/Pearl_Change_Your_Mind_Requested_by_RylerGamerDBS.png/revision/latest?cb=20210102164005&path-prefix=pt-br",
+			hp: 500,
+			atck: 50
+		},
+		{
+			name: "Ametista",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/7/75/Amethyst_Change_Your_Mind_Outfit_by_TheOffColors.png/revision/latest?cb=20210102164115&path-prefix=pt-br",
+			hp: 400,
+			atck: 25
+		},
+		{
+			name: "Steven",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/7/7e/Steven_Universe_Age_16_Two_by_TheOffColors.png/revision/latest?cb=20200907132123&path-prefix=pt-br",
+			hp: 350,
+			atck: 50
+		}
+		,
+		{
+			name: "Peridot",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/2/27/Peridot_Season_Six_Render_by_TheOffColors.png/revision/latest?cb=20200905181358&path-prefix=pt-br",
+			hp: 300,
+			atck: 100
+		}
+		,
+		{
+			name: "Lapis Lazuli",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/b/b8/Lapis_Lazuli_Season_Six_by_TheOffColors.png/revision/latest?cb=20200830181430&path-prefix=pt-br",
+			hp: 500,
+			atck: 75
+		}
+		,
+		{
+			name: "Bismuto",
+			image: "https://static.wikia.nocookie.net/stevenuniverso/images/6/66/Bismuth_S.U_The_Movie_Form_Regeneration_by_CGBomniverse.png/revision/latest?cb=20200915163209&path-prefix=pt-br",
+			hp: 600,
+			atck: 50
+		}
+	]
+
+
+
+	const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+	const gameContract = await gameContractFactory
+		.deploy(characters.map(({name})=>name),
+				characters.map(({ image }) => image),
+				characters.map(({ hp }) => hp),
+				characters.map(({ atck }) => atck));
+	await gameContract.deployed();
+	console.log("Contrato implantado no endereço:", gameContract.address);
 };
 
 const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 runMain();
